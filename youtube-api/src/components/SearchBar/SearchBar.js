e8912e98efeecf765e2568f8c6c4162924e7b031
James Basile <jbasile145@gmail.com>
2019-08-15 15:26:13 -0400

    added searchbar component

---
+++ youtube-api/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: ''
+        };
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.search)
+    }
+
+
+
+    render() {
+        return(
+            <div className='search-bar'>
+                <input 
+                value={this.state.search}
+                onChange={e => this.setState({
+                    search: e.target.value
+                })}
+                />
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar;
